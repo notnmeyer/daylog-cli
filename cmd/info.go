459351cfb4ae082c171c57969650f54e2a90a407
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2024-01-21 12:51:37 -0800

    add descriptions
    add descriptions

--- cmd/info.go
+++ cmd/info.go
@@ -11,13 +11,8 @@ import (
 // infoCmd represents the info command
 var infoCmd = &cobra.Command{
 	Use:   "info",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Display the full path to the logs directory.",
+	Long:  "Display the full path to the logs directory.",
 	Run: func(cmd *cobra.Command, args []string) {
 		dl, err := daylog.New(args, config.Project)
 		if err != nil {
