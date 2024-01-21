2dcab4eab06427defb21e55f76b5b016470f4240
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2024-01-21 12:55:02 -0800

    feat: info cmd (#29)

---
+++ cmd/info.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/notnmeyer/daylog-cli/internal/daylog"
+	"github.com/spf13/cobra"
+)
+
+var infoCmd = &cobra.Command{
+	Use:   "info",
+	Short: "Display the full path to the logs directory.",
+	Long:  "Display the full path to the logs directory.",
+	Run: func(cmd *cobra.Command, args []string) {
+		dl, err := daylog.New(args, config.Project)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Log location: %s\n", dl.ProjectPath)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(infoCmd)
+}
