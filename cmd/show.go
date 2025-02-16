f3b4e79b43e769c79dd105d72a3c429fa1bab1de
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2025-02-16 13:06:15 -0800

    feat: previous log (#41)

--- cmd/show.go
+++ cmd/show.go
@@ -3,8 +3,10 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"path/filepath"
 
 	"github.com/notnmeyer/daylog-cli/internal/daylog"
+	"github.com/notnmeyer/daylog-cli/internal/file"
 	"github.com/spf13/cobra"
 )
 
@@ -24,12 +26,25 @@ var showCmd = &cobra.Command{
 
 		format, err := cmd.PersistentFlags().GetString("output")
 		if err != nil {
-			log.Fatalf(err.Error())
+			log.Fatalf("%s", err.Error())
+		}
+
+		showPrevious, err := cmd.Root().PersistentFlags().GetBool("prev")
+		if err != nil {
+			log.Fatalf("%s", err.Error())
+		}
+
+		if showPrevious {
+			prev, err := file.PreviousLog(dl.ProjectPath, file.LogProvider{})
+			if err != nil {
+				log.Fatal(err)
+			}
+			dl.Path = filepath.Join(dl.ProjectPath, prev, "log.md")
 		}
 
 		logContents, err := dl.Show(format)
 		if err != nil {
-			log.Fatalf(err.Error())
+			log.Fatalf("%s", err.Error())
 		}
 
 		fmt.Println(string(logContents))
---
+++ internal/file/file.go
@@ -0,0 +1,119 @@
+package file
+
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"slices"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/notnmeyer/daylog-cli/internal/dateutil"
+)
+
+type FileLogProvider interface {
+	GetLogs(string) ([]string, error)
+}
+
+type LogProvider struct{}
+
+func NewLogProvider() LogProvider {
+	return LogProvider{}
+}
+
+func (LogProvider) GetLogs(projectPath string) ([]string, error) {
+	var validFiles []string
+	err := filepath.Walk(projectPath, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if path == projectPath || info.Name() == ".git" {
+			return nil
+		}
+
+		if info.Mode().IsRegular() {
+			relPath, err := filepath.Rel(projectPath, path)
+			if err != nil {
+				return err
+			}
+
+			parts := strings.Split(relPath, string(filepath.Separator))
+			if len(parts) > 0 && isYearDirectory(parts[0]) {
+				if isValidFile(path) {
+					log := convertLogToDisplayName(relPath)
+					// insert at the front of the slice
+					validFiles = slices.Insert(validFiles, 0, log)
+				}
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return validFiles, nil
+}
+
+func PreviousLog(path string, provider FileLogProvider) (string, error) {
+	// get all the logs
+	logs, err := provider.GetLogs(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// find the most recent existing log before today
+	prev, exists := findPreviousLog(logs)
+	if !exists {
+		return "", fmt.Errorf("no previous log found")
+	}
+
+	return prev, nil
+}
+
+func isYearDirectory(s string) bool {
+	if len(s) != 4 {
+		return false
+	}
+
+	if _, err := strconv.Atoi(s); err != nil {
+		return false
+	}
+
+	return true
+}
+
+func isValidFile(path string) bool {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return false
+	}
+	return len(content) > 0 && strings.TrimSpace(string(content)) != ""
+}
+
+// converts 2025/12/02/log.md to 2025/12/02 which can be used directly when editing or showing a log
+func convertLogToDisplayName(log string) string {
+	split := strings.Split(log, string(filepath.Separator))
+	return path.Join(split[0], split[1], split[2])
+}
+
+// TODO: write test
+// logs is the list of keys of all logs. each log is YYYY/MM/DD, which the most recent date at index 0
+func findPreviousLog(logs []string) (string, bool) {
+	todayTime, err := time.Parse("2006/01/02", dateutil.GetCurrent().String())
+	if err != nil {
+		return "", false
+	}
+
+	for _, log := range logs {
+		l, err := time.Parse("2006/01/02", log)
+		if err == nil && l.Before(todayTime) {
+			return log, true
+		}
+	}
+	return "", false
+}
